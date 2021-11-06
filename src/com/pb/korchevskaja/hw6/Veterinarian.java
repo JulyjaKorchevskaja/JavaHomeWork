8f4f3ab37afc6e92f189b6796c7d35e5389831b5
Korchevskaja Julyja <julija.korchevskaja@privatbank.ua>
2021-11-06 23:44:19 +0200

    Home work 6 ver. 4

--- src/com/pb/korchevskaja/hw6/Veterinarian.java
+++ src/com/pb/korchevskaja/hw6/Veterinarian.java
@@ -1,11 +1,6 @@
 package com.pb.korchevskaja.hw6;
 
 public class Veterinarian {
-//    public Object animal;
-//    public Veterinarian(){
-//    }
-//    public static void main(String[] args) {
-//    }
         public void treatAnimal (Animal animal){
             System.out.println("Food: " + animal.food + ". Location: " + animal.location);
 
